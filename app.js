c01f33b3b44d419215dd94891502fad87afcd0d2
Aal Emraan <developer.aalemraan@gmail.com>
2021-08-20 10:41:02 +0600

    clear1 button fixed

--- app.js
+++ app.js
@@ -5,10 +5,30 @@ document.getElementById('button').addEventListener('click',function(){
 
 document.getElementById('keypad').addEventListener('click',function(event){
     const number = event.target.innerText;
-    const previousNumber = document.getElementById('calc-display').value;
-    document.getElementById('calc-display').value = previousNumber + number;
+    if(!isNaN(number)){
+        const previousNumber = document.getElementById('calc-display').value;
+        document.getElementById('calc-display').value = previousNumber + number;
+    }
 })
 
 document.getElementById('submit').addEventListener('click', function(){
-    
+    const pin = document.getElementById('pin-number').value;
+    const givenPin = document.getElementById('calc-display').value;
+    if(pin == givenPin){
+        document.getElementById('success').style.display = 'block';
+        document.getElementById('error').style.display = 'none';
+    }else{
+        document.getElementById('error').style.display = 'block';
+        document.getElementById('success').style.display = 'none';
+    }
+    document.getElementById('calc-display').value = '';
+})
+
+document.getElementById('clear').addEventListener('click', function(){
+    document.getElementById('calc-display').value = '';
+})
+document.getElementById('clear1').addEventListener('click', function(){
+    const display = document.getElementById('calc-display').value;
+    const newDisplay = display.substring(0, display.length -1);
+    document.getElementById('calc-display').value = newDisplay;
 })
